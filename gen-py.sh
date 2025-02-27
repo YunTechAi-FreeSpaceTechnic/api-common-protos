2f1ccf61beb69a3e155bcc654ecd33cac6f0d2cb
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-02-27 16:40:53 +0800

    init

--- gen-py.sh
+++ gen-py.sh
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -Icommon/protos --python_out=. --grpc_python_out=. common/protos/predict.proto
+python -m grpc_tools.protoc -Iapi-common-protos --python_out=. --grpc_python_out=. api-common-protos/*.proto
