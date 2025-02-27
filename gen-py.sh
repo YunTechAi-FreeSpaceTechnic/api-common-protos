14c9b98705bad3383597c9c36402fda8b90cac33
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-02-27 21:30:54 +0800

    fix gen py bug

--- gen-py.sh
+++ gen-py.sh
@@ -1,2 +1,2 @@
 mkdir protos
-python -m grpc_tools.protoc -Iapi-common-protos --python_out=. --grpc_python_out=protos api-common-protos/*.proto
+python -m grpc_tools.protoc -Iapi-common-protos --python_out=. --grpc_python_out=. api-common-protos/*.proto
