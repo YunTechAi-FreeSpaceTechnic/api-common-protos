7bf2c85e357af44c078e4a18fa91bcbb2ae04061
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-02-27 17:01:49 +0800

    move output folder

--- gen-py.sh
+++ gen-py.sh
@@ -1,1 +1,2 @@
-python -m grpc_tools.protoc -Iapi-common-protos --python_out=. --grpc_python_out=. api-common-protos/*.proto
+mkdir protos
+python -m grpc_tools.protoc -Iapi-common-protos --python_out=protos --grpc_python_out=protos api-common-protos/*.proto
